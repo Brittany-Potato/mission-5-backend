a2380f3a5bddf0106a71a8747f8cecbad752d4d5
brittany-potato <brittanycc@missionreadyhq.com>
2025-06-20 18:39:55 +1200

    Server is running, will create my own branch now

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
-// Imports, constants and installs.
+//* ~ Imports, constants and installs. ~
 const express = require("express");
 const app = express();
-const port = " ";
+const port = "3000";
+const { MongoClient } = require("mongodb");
+const dotenv = require('dotenv');
 
-// Middleware to parse JSON requests.
+const ai_api = process.env.AI_API;
+
+
+//* ~ Middleware to parse JSON requests. ~
 app.use(express.json());
 
-// Simple GET endpoint.
-app.get("./hello", (req, res) => {
+//* ~ Simple GET endpoint. ~
+app.get("/hello", (req, res) => {
   res.json({ message: "Hello from Node.js!" });
 });
 
-// Simple POST end point.
-app.post("./echo", (req, res) => {
+//* ~ Simple POST end point. ~
+app.post("/echo", (req, res) => {
   res.json({
     message: "You sent this",
     data: req.body,
   });
 });
 
-// Start the server.
+//? ~ Brittany ~
+
+
+
+
+//? ~ Teancum ~
+
+
+
+
+
+//? ~ Afton ~
+
+
+
+
+
+//* ~ Start the server. ~
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
